2d6a7bbbbe2c534e072538e5b844ca0eb7759e39
ProbirKumare <probirsharma655@gmail.comm>
2023-09-09 21:31:33 +0600

    async user added

---
+++ js/user.js
@@ -0,0 +1,25 @@
+// console.log('async ')
+const loadUserFetch = () => {
+    const url = `https://randomuser.me/api/?gender=female`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayUser(data.results[0]))
+        .catch(error => console.log(error))
+}
+
+const loadAsyncUser = async () => {
+    const url = `https://randomuser.me/api/?gender=female`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayUser(data.results[0]);
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
+const displayUser = user => {
+    console.log(user)
+}
